258aa459b10e2980d6b6aa071afd46a7bc067d71
Ciszeg <ciszky@outlook.com>
2026-05-18 17:33:28 +0200

    Przygotowanie skryptu pod CachyOS.

--- install-apek.sh
+++ install-apek.sh
@@ -30,12 +30,12 @@ fi
 
 # Instalacja pakietów z oficjalnych repozytoriów pacman
 echo "Instalacja pakietów pierwszej ciszkowej potrzeby"
-sudo pacman -S vlc gutenprint cups system-config-printer fish ghostty kitty neovim steam steam-native-runtime lutris wine wine-mono wine-gecko mono obs-studio linux-headers v4l2loopback-dkms gamescope gameconqueror rsync dkms jdk17-openjdk bluez-utils winetricks kdenlive kdeconnect okular gwenview transmission-qt qnapi github-cli imagemagick ttf-jetbrains-mono ttf-fira-code ttf-fira-sans flatpak
+sudo pacman -S vlc gutenprint cups system-config-printer fish ghostty kitty neovim steam discord lutris wine wine-mono wine-gecko mono obs-studio linux-headers v4l2loopback-dkms gamescope gameconqueror rsync dkms jdk17-openjdk bluez-utils winetricks kdenlive kdeconnect okular gwenview transmission-gtk qnapi github-cli imagemagick ttf-jetbrains-mono ttf-fira-code ttf-fira-sans flatpak
 check_success
 
 # Instalacja reszy apek z AUR
 echo "Instalacja apek z AUR."
-yay -S zen-browser-bin vscodium-bin spotify onlyoffice-bin bpytop mangohud-git gzdoom-bin raze-bin ttf-dejavu openrgb vintagestory pnpm yt-dlp protontricks qemu docker vpcs dynamips libvirt ubridge inetutils gns3-server gns3-gui update-grub kvantum-qt6-git vesktop-bin protonup-qt xone-dlundqvist-dkms-git ttf-times-new-roman mov-cli python-mov-cli-files python-mov-cli-youtube nwg-look qt5ct qt6ct
+yay -S zen-browser-bin vscodium-bin spotify onlyoffice-bin bpytop mangohud-git ttf-dejavu openrgb pnpm yt-dlp protontricks update-grub kvantum-qt6-git protonup-qt xone-dkms-git ttf-times-new-roman nwg-look qt5ct qt6ct
 check_success
 
 # Instalacja flathub
@@ -45,5 +45,8 @@ flatpak install flathub
 echo "Ustawiam czas na lokalny."
 timedatectl set-local-rtc 1 --adjust-system-clock
 check_success
+
+# Instalacja Hyprland z konfiguracją end_4
+bash <(curl -s https://ii.clsty.link/get)
 
 echo "Instalacja zakończona pomyślnie!"
